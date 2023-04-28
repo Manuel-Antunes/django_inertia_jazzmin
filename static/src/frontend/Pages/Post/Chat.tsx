8a85fca2998e17edc20e7c82f3cd39577afbe7bd
Manuel-Antunes <nerdbroz2@gmail.com>
2023-04-28 03:46:29 -0300

    fix: remove useless components

--- static/src/frontend/Pages/Post/Chat.tsx
+++ static/src/frontend/Pages/Post/Chat.tsx
@@ -2,7 +2,7 @@ import { usePage } from "@inertiajs/react";
 import React from "react";
 
 const Chat: React.FC = () => {
-  const { props } = usePage();
+  const { props } = usePage<{ post: any }>();
   return <div>Chat do Post: {props.post.title}</div>;
 };
 
--- static/src/frontend/Pages/Testando.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Testando: React.FC = () => {
-  return <div>cu</div>;
-};
-
-export default Testando;
