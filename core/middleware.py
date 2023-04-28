da698cdfb052396c2c0d72fdd22c734d83b995d1
Manuel-Antunes <nerdbroz2@gmail.com>
2023-04-28 04:04:06 -0300

    feat: inertia share middleware

---
+++ core/middleware.py
@@ -0,0 +1,18 @@
+from inertia import share
+from django.conf import settings
+
+
+class InertiaShareMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        print(request.user.is_authenticated)
+        share(request,
+              # app_name=settings.APP_NAME,
+              user=lambda: request.user,
+              authenticated=lambda: request.user.is_authenticated,
+              )
+        response = self.get_response(request)
+        return response
--- core/settings.py
+++ core/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'inertia.middleware.InertiaMiddleware',
+    'core.middleware.InertiaShareMiddleware'
 ]
 
 ROOT_URLCONF = 'core.urls'
